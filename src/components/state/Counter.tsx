9a6e97feab03520b7ea4780cca0bb899f7a586e5
Oludire Damilare <oludire@gmail.com>
2025-01-13 00:13:36 +0100

    ✅useReducer strict action types

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,7 +4,17 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = { type: string; payload: number };
+type UpdateAction = {
+  type: 'increment' | 'decrement';
+  payload: number;
+};
+
+type ResetAction = {
+  type: 'reset';
+};
+
+// type CounterAction = { type: 'increment' | 'decrement' | 'reset'; payload: number };
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 0 };
 
@@ -14,19 +24,22 @@ function counterReducer(state: CounterState, action: CounterAction) {
       return { ...state, count: state.count - action.payload };
     case 'increment':
       return { ...state, count: state.count + action.payload };
+    case 'reset':
+      return initialState;
     default:
       throw new Error('unknown action');
   }
 }
 
 function Counter() {
-  const [state, dispatch] = useReducer(counterReducer, initialState);
+  const [{ count }, dispatch] = useReducer(counterReducer, initialState);
   return (
     <div>
       <h1>Welcome to my counter</h1>
-      <p>Count: {state.count}</p>
+      <p>Count: {count}</p>
       <button onClick={() => dispatch({ type: 'decrement', payload: 1 })}>&minus;</button>
       <button onClick={() => dispatch({ type: 'increment', payload: 1 })}>&plus;</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset♻️</button>
     </div>
   );
 }
